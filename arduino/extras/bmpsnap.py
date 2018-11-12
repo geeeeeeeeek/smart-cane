d3fd94115344117c00ca0ab15060887e81d1c181
nicktogo <pingshal@andrew.cmu.edu>
2018-11-11 20:56:50 -0800

    ultrasonic & detection
    add requirements
    
    ml
    
    update gitignore
    
    remove model

--- arduino/extras/bmpsnap.py
+++ arduino/extras/bmpsnap.py
@@ -17,81 +17,116 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with BreezyArduCAM.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
+import subprocess
 import time
 import serial
 from sys import stdout
 
 from helpers import *
+from detection import *
 
 # Modifiable params -------------------------------------------------------------------
 
-#PORT = '/dev/ttyACM0' # Ubuntu
+# PORT = '/dev/ttyACM0' # Ubuntu
 # PORT = 'COM9'         # Windows
-PORT = '/dev/tty.usbmodem14111'
+CAMERA_PORT = '/dev/tty.usbmodem1431'
+SENSOR_PORT = '/dev/tty.usbmodem14111'
 
-BAUD = 921600       # Change to 115200 for Due
+CAMERA_BAUD = 921600
+SENSOR_BAUD = 9600
 
 OUTFILENAME = 'test.bmp'
+
 
 # helpers  --------------------------------------------------------------------------
 
 def num2bytes(n):
-    return [n&0xFF, (n>>8)&0xFF, (n>>16)&0xFF, (n>>24)&0XFF]
+    return [n & 0xFF, (n >> 8) & 0xFF, (n >> 16) & 0xFF, (n >> 24) & 0XFF]
+
 
 def dump(msg):
     stdout.write(msg)
     stdout.flush()
 
+
 # BMP header for 320x240 image ------------------------------------------------------
-#
 # See: http://www.fastgraph.com/help/bmp_header_format.html
 # See: https://upload.wikimedia.org/wikipedia/commons/c/c4/BMPfileFormat.png
-
 header = [
-    0x42, 0x4D,             # signature, must be 4D42 hex
-    0x36, 0x58, 0x02, 0x00, # size of BMP file in bytes (unreliable)
-    0x00, 0x00,             # reserved, must be zero
-    0x00, 0x00,             # reserved, must be zero
-    0x42, 0x00, 0x00, 0x00, # offset to start of image data in bytes
-    0x28, 0x00, 0x00, 0x00, # size of BITMAPINFOHEADER structure, must be 40
-    0x40, 0x01, 0x00, 0x00, # image width in pixels
-    0xF0, 0x00, 0x00, 0x00, # image height in pixels
-    0x01, 0x00,             # number of planes in the image, must be 1
-    0x10, 0x00,             # number of bits per pixel
-    0x03, 0x00, 0x00, 0x00, # compression type
-    0x00, 0x58, 0x02, 0x00, # size of image data in bytes (including padding)
-    0xC4, 0x0E, 0x00, 0x00, # horizontal resolution in pixels per meter (unreliable)
-    0xC4, 0x0E, 0x00, 0x00, # vertical resolution in pixels per meter (unreliable)
-    0x00, 0x00, 0x00, 0x00, # number of colors in image, or zero
-    0x00, 0x00, 0x00, 0x00, # number of important colors, or zero
-    0x00, 0xF8, 0x00, 0x00, # red channel bitmask
-    0xE0, 0x07, 0x00, 0x00, # green channel bitmask
+    0x42, 0x4D,  # signature, must be 4D42 hex
+    0x36, 0x58, 0x02, 0x00,  # size of BMP file in bytes (unreliable)
+    0x00, 0x00,  # reserved, must be zero
+    0x00, 0x00,  # reserved, must be zero
+    0x42, 0x00, 0x00, 0x00,  # offset to start of image data in bytes
+    0x28, 0x00, 0x00, 0x00,  # size of BITMAPINFOHEADER structure, must be 40
+    0x40, 0x01, 0x00, 0x00,  # image width in pixels
+    0xF0, 0x00, 0x00, 0x00,  # image height in pixels
+    0x01, 0x00,  # number of planes in the image, must be 1
+    0x10, 0x00,  # number of bits per pixel
+    0x03, 0x00, 0x00, 0x00,  # compression type
+    0x00, 0x58, 0x02, 0x00,  # size of image data in bytes (including padding)
+    0xC4, 0x0E, 0x00, 0x00,  # horizontal resolution in pixels per meter (unreliable)
+    0xC4, 0x0E, 0x00, 0x00,  # vertical resolution in pixels per meter (unreliable)
+    0x00, 0x00, 0x00, 0x00,  # number of colors in image, or zero
+    0x00, 0x00, 0x00, 0x00,  # number of important colors, or zero
+    0x00, 0xF8, 0x00, 0x00,  # red channel bitmask
+    0xE0, 0x07, 0x00, 0x00,  # green channel bitmask
     0x1F, 0x00, 0x00, 0x00  # blue channel bitmask
 ]
 
 # main ------------------------------------------------------------------------------
+MIN_DISTANCE_TO_ALERT = 5
+
+
+def audio_output(content):
+    subprocess.call(["say", content])
+
+
+def handle_sensor_output(line):
+    if not len(line):
+        return
+
+    try:
+        sensors = sensorAck.split(";")
+    except IOError as e:
+        return
+
+    if len(sensors) < 2:
+        return
+    left, right = int(sensors[0]), int(sensors[1])
+    if left < MIN_DISTANCE_TO_ALERT and right < MIN_DISTANCE_TO_ALERT:
+        audio_output("Obstacle on both sides")
+    elif left < MIN_DISTANCE_TO_ALERT:
+        audio_output("Obstacle on your left")
+    elif right < MIN_DISTANCE_TO_ALERT:
+        audio_output("Obstacle on your right")
+
 
 if __name__ == '__main__':
 
     # Open connection to Arduino with a timeout of two seconds
-    port = serial.Serial(PORT, BAUD, timeout=2)
+    cameraPort = serial.Serial(CAMERA_PORT, CAMERA_BAUD, timeout=2)
+    sensorPort = serial.Serial(SENSOR_PORT, SENSOR_BAUD, timeout=2)
 
     dump('Starting capture ...')
 
     # Report acknowledgment from camera
-    getack(port)
+    getack(cameraPort)
 
     # Wait a spell
     time.sleep(0.2)
 
     while True:
-        ack = port.readline().decode()
+        getack(sensorPort)
+        cameraAck = cameraPort.readline().decode()
+        sensorAck = sensorPort.readline().decode()
 
-        if ack == 'capture enabled\r\n':
-            print(ack)
+        handle_sensor_output(sensorAck)
+
+        if cameraAck == 'capture enabled\r\n':
+            print(cameraAck)
             # Send "start capture" message
-            sendbyte(port, 1)
+            sendbyte(cameraPort, 1)
 
             dump('\nWriting file %s (may appear upside-down) ...' % OUTFILENAME)
 
@@ -102,13 +137,17 @@ if __name__ == '__main__':
             outfile.write(bytearray(header))
 
             # Read bytes from serial and write them to file
-            for k in range(320*240):
-                c = outfile.write(port.read(2))
+            for k in range(320 * 240):
+                c = outfile.write(cameraPort.read(2))
 
             # Send "stop" message
-            sendbyte(port, 0)
+            sendbyte(cameraPort, 0)
 
             # Close output file
             outfile.close()
 
             print('\nDone')
+
+            detections = detect(OUTFILENAME)
+            objects = ','.join(eachObject['name'] for eachObject in detections)
+            audio_output('{} are in front of you'.format(objects))
---
+++ arduino/extras/detection.py
@@ -0,0 +1,15 @@
+from imageai.Detection import ObjectDetection
+import os
+
+
+def detect(imagename):
+    execution_path = os.getcwd()
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, imagename),
+                                                 output_image_path=os.path.join(execution_path, 'detected_' + imagename))
+
+    return detections
