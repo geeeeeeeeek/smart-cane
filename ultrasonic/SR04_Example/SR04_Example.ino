7aa6110df26595e77f062c1ef42ed8c2d9a14289
nicktogo <pingshal@andrew.cmu.edu>
2018-11-11 21:46:54 -0800

    update ino

--- ultrasonic/SR04_Example/SR04_Example.ino
+++ ultrasonic/SR04_Example/SR04_Example.ino
@@ -3,8 +3,13 @@
 #include "SR04.h"
 #define TRIG_PIN 12
 #define ECHO_PIN 11
+
+#define TRIG_PIN_2 10
+#define ECHO_PIN_2 9
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+SR04 sr04_2 = SR04(ECHO_PIN_2,TRIG_PIN_2);
 long a;
+long b;
 
 void setup() {
    Serial.begin(9600);
@@ -13,7 +18,11 @@ void setup() {
 
 void loop() {
    a=sr04.Distance();
+   b=sr04_2.Distance();
+
    Serial.print(a);
-   Serial.println("cm");
-   delay(1000);
+   Serial.print(";");
+   Serial.print(b);
+   Serial.println("\n");
+   delay(500);
 }
